c27d6ea9f7bd52c2cdc86a100b40a97ba8332a82
Michaël Le Marec <michael.lemarec@turbonomic.com>
2023-03-20 14:49:43 +0100

    Added vscode installer

---
+++ install-vscode.sh
@@ -0,0 +1,13 @@
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+sudo tee /etc/yum.repos.d/vscode.repo <<ADDREPO
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+ADDREPO
+
+sudo dnf install code
+

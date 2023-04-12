eb479c34a79f288feca2fd83b7718cf46d0ccc98
Michaël Le Marec <lemarec@gmail.com>
2023-04-12 09:50:32 -0400

    update

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,14 +1,22 @@
-# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-# sudo tee /etc/yum.repos.d/vscode.repo <<ADDREPO
-# [code]
-# name=Visual Studio Code
-# baseurl=https://packages.microsoft.com/yumrepos/vscode
-# enabled=1
-# gpgcheck=1
-# gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-# ADDREPO
+sudo tee /etc/yum.repos.d/vscode.repo <<ADDREPO
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+ADDREPO
 
-# sudo dnf install code -y
+sudo dnf install code -y
 
-cat keybindings.json > ~/.config/VSCodium/User/keybindings.json
+cat keybindings.json > ~/.config/Code/User/keybindings.json
+
+git config --global user.name "Michaël Le Marec"
+git config --global user.email "lemarec@gmail.com"
+git config --global --list
+
+token=$(cat token.tk)
+
+git clone https://michael-le-marec@$token@github.com/michael-le-marec/redhat-do180.git

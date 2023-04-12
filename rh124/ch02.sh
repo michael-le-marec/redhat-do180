0a3ad15069398f41928a24b7fc18b2710d5702a4
Student User <student@workstation.lab.example.com>
2023-04-12 09:36:30 -0400

    rh124-02

--- rh124/ch02.sh
+++ rh124/ch02.sh
@@ -10,3 +10,39 @@ whoami ; hostname
 date
 date +%R
 date +%x
+
+file /etc/passwd
+file /bin/passwd
+file /home
+
+ll
+cat install-vscode.sh keybindings.json
+
+head keybindings.json
+tail -n 3 keybindings.json
+
+wc keybindings.json
+wc -l -w -c keybindings.json # is the same
+
+ls --"TAB" "TAB"
+
+history
+!wc # launch last wc command
+
+# Ctrl+R to reverse search
+# Ctrl+K to get last argument
+
+
+lab start cli-review
+date
+date +%R
+
+file ~/zcat
+wc ~/zcat
+head ~/zcat
+head -4 ~/zcat
+tail -n 20 ~/zcat
+history
+!23
+lab finish cli-review
+
